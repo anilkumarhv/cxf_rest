63259b8b52a89b556e4d2547669566c2f4306125
anilkumarhv <anilkumarhv04@gmail.com>
2016-08-25 19:41:59 +0530

    Add files via upload

---
+++ src/main/java/model/User.java
@@ -0,0 +1,72 @@
+package model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "user")
+public class User {
+
+    private Integer id;
+
+    private String  name;
+    public User() {}
+    public User(String id, String name) {
+    	if(!id.isEmpty())
+    		this.id = Integer.parseInt(id);
+    	this.name =name;
+    	
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        return name != null ? name.equals(user.name) : user.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+//    @Override
+//    public String toString() {
+//        return String.format("{id=%s,name=%s}", id, name);
+//    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
